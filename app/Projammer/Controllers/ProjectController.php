34adaa1ef7a573d88ef21ac2b1718a7a57d8e70f
Alex <alex@wearenotmachines.com>
2014-10-26 01:28:06 +0100

    Stopped using editor in project overview screen and inlined the markup instead  at this stage can't see the Projects editor being used separately from this screen.

--- app/Projammer/Controllers/ProjectController.php
+++ app/Projammer/Controllers/ProjectController.php
@@ -19,7 +19,6 @@ class ProjectController extends ProjammerController {
 	 */
 	public function index()	{
 		$this->_data['projectStatuses'] = array_combine(Project::getProjectStatuses(), array_map(function($element) { return ucfirst($element); }, Project::getProjectStatuses()));
-		$this->_data['projectEditor'] = Project::getEditor(Project::first());
 		$this->layout->content = View::make("projects.index", $this->_data);
 	}
 
--- app/Projammer/Views/components/projects/editor.php
+++ app/Projammer/Views/components/projects/editor.php
@@ -1,21 +1,27 @@
 <?php namespace Projammer\Models;
 use Form;
 ?>
-<div class="newProject" ng-controller="ProjectEditorController">
-	<form>
-	<div>
-		<label>Project Name</label>
-		<input type="text" ng-model="project.name" name="name" autocomplete="off" placeholder="the label for the project" value="TESTING" />
-	</div>
-	<div>
-		<label>Project Description</label>
-		<textarea ng-model="project.description" name="description" placeholder="a brief description"></textarea>
-	</div>
-	<div>
-		<label>Project Status</label>
-		<?= Form::select("status", array_combine(Project::getProjectStatuses(),Project::getProjectStatuses()), "presales", array("autocomplete"=>"off", "ng-model"=>"project.status")); ?>
-	</div>
+<div class="editorContainer" ng-controller="ProjectsController">
+	<a href="" ng-click="resetCurrentProject();showEditor = true">Create Project</a>
+	<div class="newProject" ng-show="showEditor">
+		<form>
+		<input type="hidden" name="id" ng-model="currentProject.id" />
+		<div>
+			<label>Project Name</label>
+			<input type="text" ng-model="currentProject.name" name="name" autocomplete="off" placeholder="the label for the project" />
+		</div>
+		<div>
+			<label>Project Description</label>
+			<textarea ng-model="currentProject.description" name="description" placeholder="a brief description"></textarea>
+		</div>
+		<div>
+			<label>Project Status</label>
+			<?= Form::select("status", array_combine(Project::getProjectStatuses(),Project::getProjectStatuses()), "presales", array("autocomplete"=>"off", "ng-model"=>"currentProject.status")); ?>
+		</div>
 
-	<button ng-click="createProject()">Save</button>
-	</form>
+		<button ng-click="updateProject()">Save</button>
+		</form>
+		<button ng-click="test()">Test</button>
+	</div>
+		{{currentProject}}
 </div>
