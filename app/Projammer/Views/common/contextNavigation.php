b239edc9d58b98997ca2351e6ff4d645e009249e
Alex <alex@wearenotmachines.com>
2014-10-26 11:18:20 +0000

    Navigation link for project summary

--- app/Projammer/Views/common/contextNavigation.php
+++ app/Projammer/Views/common/contextNavigation.php
@@ -1,6 +1,6 @@
 <div id="contextNavigation">
 <ul class="nav nav-pills nav-justified" role="tablist">
-	<li<?= $active=="definition" ? " class='active'" : ""; ?>><a href="#">Definition</a></li>
+	<li<?= $active=="definition" ? " class='active'" : ""; ?>><a href="/project/<?= $project->id; ?>">Definition</a></li>
 	<li<?= $active=="requirements" ? " class='active'" : ""; ?>><a href="/project/<?= $project->id; ?>/requirements">Requirements</a></li>
 	<li<?= $active=="deliverables" ? " class='active'" : ""; ?>><a href="#">Deliverables</a></li>
 	<li<?= $active=="iterations" ? " class='active'" : ""; ?>><a href="#">Iterations</a></li>
